19a166cf566fb002ba042bb849578b563e3b590b
raduubraduu <lefter_radu@yahoo.com>
2023-07-02 18:39:43 +0100

    added functionality to store modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 const SetupPage = () => {
-    return (
-      <div className="p-4">
-        <Modal title="Test" description="Test description" isOpen onClose={()=>{}}>Children</Modal>
-      </div>
-    )
-  }
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return null;
+};
 
 export default SetupPage;
